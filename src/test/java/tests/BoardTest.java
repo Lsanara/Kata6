2792a1e5bc2caa79c213494bd329d0f045ae1799
Lsanara <nebeproyects@gmail.com>
2024-06-30 10:06:58 +0100

    Third commit

---
+++ src/test/java/tests/BoardTest.java
@@ -0,0 +1,87 @@
+package tests;
+
+import org.junit.Test;
+import kata6.Board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static tests.BoardTest.Cases.*;
+
+public class BoardTest {
+    @Test
+    public void should_return_no_winner_given_empty_board_3x3(){
+        Board board = new Board("");
+        board.chechWinner();
+        String winner = board.winner();
+        assertThat(winner).isEqualTo("");
+    }
+
+    @Test
+    public void should_return_winner_given_board_3x3_vertical_line(){
+        Board board = new Board(board_3x3_vertical_line);
+        board.chechWinner();
+        String winner = board.winner();
+        assertThat(winner).isEqualTo("O");
+    }
+
+    @Test
+    public void should_return_winner_given_board_3x3_horizontal_line(){
+        Board board = new Board(board_3x3_horizontal_line);
+        board.chechWinner();
+        String winner = board.winner();
+        assertThat(winner).isEqualTo("O");
+    }
+
+    @Test
+    public void should_return_winner_given_board_3x3_descending_diagonal_line(){
+        Board board = new Board(board_3x3_descending_diagonal_line);
+        board.chechWinner();
+        String winner = board.winner();
+        assertThat(winner).isEqualTo("X");
+    }
+
+    @Test
+    public void should_return_winner_given_board_3x3_ascending_diagonal_line(){
+        Board board = new Board(board_3x3_ascending_diagonal_line);
+        board.chechWinner();
+        String winner = board.winner();
+        assertThat(winner).isEqualTo("X");
+    }
+
+    @Test
+    public void should_return_draw_given_board_3x3_draw(){
+        Board board = new Board(board_3x3_draw);
+        board.chechWinner();
+        String winner = board.winner();
+        assertThat(winner).isEqualTo("draw");
+    }
+    public static class Cases {
+        public static String board_3x3_horizontal_line = """
+                O.X
+                OOO
+                .XX
+                """.trim();
+
+        public static String board_3x3_vertical_line = """
+                X.O
+                .XO
+                OXO
+                """.trim();
+
+        public static String board_3x3_descending_diagonal_line= """
+                XOO
+                OXO
+                OOX
+                """.trim();
+        public static String board_3x3_ascending_diagonal_line= """
+                OOX
+                OXO
+                XOO
+                """.trim();
+        public static String board_3x3_draw= """
+                XOO
+                OXX
+                XOO
+                """.trim();
+    }
+}
+
